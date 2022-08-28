0e7f804a00d28775c41e7d305495ee3f5b7ee4b2
Ryan <keathley.joseph.ryan@gmail.com>
2022-08-27 23:46:36 -0700

    broken, bad learning

--- rnad.py
+++ rnad.py
@@ -18,9 +18,9 @@ class Net (torch.nn.Module):
     def __init__ (self):
         super().__init__()
 
-        self.fc0 = nn.Linear(size**2, size**2)
-        self.fc1 = nn.Linear(size**2, size)
-        self.fc2 = nn.Linear(size**2, 1)
+        self.fc0 = nn.Linear(size**2, size**3)
+        self.fc1 = nn.Linear(size**3, size)
+        self.fc2 = nn.Linear(size**3, 1)
 
     def forward (self, input):
         tower = torch.relu(self.fc0(input.view(-1, size**2)))
@@ -48,6 +48,9 @@ class RNaD:
         self.gamma = .1
         self.total_NeuRD_steps = 2**10
         self.total_RNaD_steps = 2**10
+    
+    def reset_net (self):
+        self.net = Net()
 
     def generate_input_batch (self, batch_size : int, matrix_entry_distribution=None) -> torch.tensor :
         #currenly bypasses generator and uses normal
@@ -97,7 +100,7 @@ class RNaD:
 
         return expl
 
-    def exploitability_demo (self, total_batches_exp, steps_exp, lr=.01):
+    def exploitability_demo_PG (self, total_batches_exp, steps_exp, lr=.01, update_rule='neurd'):
         #plot the mean exploitability on validation batch
 
         optimizer = torch.optim.SGD(x.net.parameters(), lr)
@@ -108,6 +111,8 @@ class RNaD:
         for steps in range(2**steps_exp):
             for _ in range(2**(total_batches_exp-steps_exp)):
                 optimizer.zero_grad()
+
+
                 loss = x.compute_loss_from_PG_step()
 
                 loss.backward()
@@ -126,9 +131,42 @@ class RNaD:
         fig, ax = plt.subplots()
         ax.plot(range(2**steps_exp), mean_expls)
         ax.set_ylim([0, 4])
-        # ax.plot()
-        plt.show()    
 
+        plt.show() 
+        print(mean_expls[-1])
+        return   
+
+    def exploitability_demo_NeuRD (self, total_batches_exp, steps_exp, lr=.01):
+        #plot the mean exploitability on validation batch
+
+        optimizer = torch.optim.SGD(x.net.parameters(), lr)
+        
+        mean_expls = []
+        policy_batch_total = torch.zeros(self.validation_batch.shape[:2])
+        policy_batch_checkpoints = []
+        for steps in range(2**steps_exp):
+            for _ in range(2**(total_batches_exp-steps_exp)):
+                optimizer.zero_grad()
+                loss = x.compute_loss_from_NeuRD_step()
+
+                loss.backward()
+                optimizer.step()
+
+            logits_batch, policy_batch, value_batch, expl = self.exploitability_(self.validation_batch)
+            policy_batch_total += policy_batch
+            time_averaged_policy = policy_batch_total / (steps + 1)
+
+            expl = self.exploitability(self.validation_batch, time_averaged_policy)
+
+            policy_batch_checkpoints.append(policy_batch)
+            mean_expls.append(torch.mean(expl).item())
+            # mean exploitability of mean (time averaged) strategies
+        
+        fig, ax = plt.subplots()
+        ax.plot(range(2**steps_exp), mean_expls)
+        ax.set_ylim([0, 4])
+        plt.show()    
+        print(mean_expls[-1])
 
     def compute_loss_from_PG_step (self) :
 
@@ -147,87 +185,175 @@ class RNaD:
         
         advantages = (rewards - value_batch).flatten()
 
-        baseline_loss = torch.sum(advantages**2) / 2
+        baseline_loss = torch.mean(advantages**2)
 
-        entropy_loss = torch.sum(policy_batch * F.log_softmax(logits_batch, dim=1))
+        entropy_loss = torch.mean(policy_batch * F.log_softmax(logits_batch, dim=1))
 
         cross_entropy = F.nll_loss(input=F.log_softmax(logits_batch, dim=1), target=actions.flatten(), reduction="none")
-        policy_loss = torch.sum(cross_entropy * advantages.detach())
+        policy_loss = torch.mean(cross_entropy * advantages.detach())
 
         loss = baseline_loss + policy_loss + entropy_loss
         return loss
 
-    def print (self, input_batch) :
+    def compute_loss_from_NeuRD_step (self) :
+
+        input_batch = self.generate_input_batch(self.batch_size)
         logits_batch, policy_batch, value_batch = self.net.forward(input_batch)
 
-        
-        # print("M:")
-        # print(input_batch)
-        # print("Policy Batch:")
-        print(policy_batch)
+        try:
+            actions = torch.multinomial(policy_batch, num_samples=1)
+        except:
+            print(policy_batch)
 
-    def RPS_animation (self, total_batches_exp, steps_exp):
-        triangle = np.array([[0, 1], [1, 0], [0, 0],[0, 1]])
-        triangle_x = triangle[:, 0]
-        triangle_y = triangle[:, 1]
-    
-        p_x = []
-        p_y = []
+        A = torch.squeeze(input=actions[:self.batch_size], dim=1)
+        B = torch.squeeze(input=actions[self.batch_size:], dim=1)
+        M = input_batch[:self.batch_size]
+
+        player_one_rewards = M[torch.arange(self.batch_size), A][torch.arange(self.batch_size), B].view(self.batch_size, 1)
+        rewards = torch.cat((player_one_rewards, -player_one_rewards), dim=0)
+        advantages = (rewards - value_batch).flatten()
+        chosen_logit_batch = logits_batch[torch.arange(self.batch_size * 2), torch.squeeze(input=actions, dim=1)].flatten()
+        chosen_policy_batch = policy_batch[torch.arange(self.batch_size * 2), torch.squeeze(input=actions, dim=1)].flatten()
+
+        policy_loss = torch.mean(-chosen_logit_batch * advantages.detach() / chosen_policy_batch.detach())
+
+        baseline_loss = torch.mean(advantages**2) / 2
+
+        entropy_loss = torch.mean(policy_batch * F.log_softmax(logits_batch, dim=1))
+
+        loss = baseline_loss + policy_loss #+ entropy_loss
+        return loss
 
 
-        RPS = torch.tensor([[0,-1,1],[1,0,-1],[-1,1,0]], dtype=torch.float).unsqueeze(dim=0)
-
-        optimizer = torch.optim.SGD(x.net.parameters(), lr=.01)
-        
-        for steps in range(2**steps_exp):
-            for _ in range(2**(total_batches_exp-steps_exp)):
-                optimizer.zero_grad()
-                loss = x.compute_loss_from_PG_step()
-
-                loss.backward()
-                optimizer.step()
-
-            with torch.no_grad():
-                logits_batch, policy_batch, value_batch = x.net.forward(RPS)
-                p_x.append(policy_batch[0, 0])
-                p_y.append(policy_batch[0, 1])
-        fig, ax = plt.subplots()
-
-        # Convert to time average
-        p_x = [sum(p_x[:_]) / _ for _ in range(1, 2**steps_exp+1)]
-        p_y = [sum(p_y[:_]) / _ for _ in range(1, 2**steps_exp+1)]
-        steps_text = ["Param update steps: {}".format((_+1) * 2**(total_batches_exp-steps_exp)) for _ in range(2**steps_exp)]
-
-        def animate(i):
-            length = 10
-            # q_x = p_x[i-length: i]
-            # q_y = p_y[i-length: i]
-
-            ax.clear()
-            ax.set_xlim([0, 1])
-            ax.set_ylim([0, 1])
-            ax.text(.6, .6, steps_text[i])
-
-            for _ in range(length):
-                z = max(0, i+_-length)
-                ax.plot(p_x[z:z+2], p_y[z:z+2], alpha=(_/length), color='black')
-            
-            # ax.plot(q_x, q_y, alpha=.2)
-            
-            ax.plot(triangle_x, triangle_y)
-            ax.plot([1/3], [1/3], '--bo')
-
-        print("Final mean", torch.mean(torch.tensor(p_x)), torch.mean(torch.tensor(p_y)))
-
-        ani = animation.FuncAnimation(fig, animate, frames=2**steps_exp, interval=6, repeat=True)
-        # writergif = animation.PillowWriter(fps=30)
-        # ani.save("/home/user/Desktop/a.gif", writer=writergif)
-        plt.show()    
-
+ 
 
 if __name__ == '__main__':
 
     #plot shit
     x = RNaD()
-    # x.RPS_animation(12, 9)
-    x.exploitability_demo(total_batches_exp=14, steps_exp=8, lr=.001)
+    x.batch_size = 2**7
+    n = 10
+    x.exploitability_demo_NeuRD(n, n, .01)
+    x.reset_net()
+    x.exploitability_demo_PG(n, n, .01)
+
+
+#     def print (self, input_batch) :
+#         logits_batch, policy_batch, value_batch = self.net.forward(input_batch)
+#         print(policy_batch)
+
+#     def RPS_animation (self, total_batches_exp, steps_exp):
+#         triangle = np.array([[0, 1], [1, 0], [0, 0],[0, 1]])
+#         triangle_x = triangle[:, 0]
+#         triangle_y = triangle[:, 1]
+    
+#         p_x = []
+#         p_y = []
+
+
+#         RPS = torch.tensor([[0,-1,1],[1,0,-1],[-1,1,0]], dtype=torch.float).unsqueeze(dim=0)
+
+#         optimizer = torch.optim.SGD(x.net.parameters(), lr=.01)
+        
+#         for steps in range(2**steps_exp):
+#             for _ in range(2**(total_batches_exp-steps_exp)):
+#                 optimizer.zero_grad()
+#                 loss = x.compute_loss_from_PG_step()
+
+#                 loss.backward()
+#                 optimizer.step()
+
+#             with torch.no_grad():
+#                 logits_batch, policy_batch, value_batch = x.net.forward(RPS)
+#                 p_x.append(policy_batch[0, 0])
+#                 p_y.append(policy_batch[0, 1])
+#         fig, ax = plt.subplots()
+
+#         # Convert to time average
+#         p_x = [sum(p_x[:_]) / _ for _ in range(1, 2**steps_exp+1)]
+#         p_y = [sum(p_y[:_]) / _ for _ in range(1, 2**steps_exp+1)]
+#         steps_text = ["Param update steps: {}".format((_+1) * 2**(total_batches_exp-steps_exp)) for _ in range(2**steps_exp)]
+
+#         def animate(i):
+#             length = 10
+#             # q_x = p_x[i-length: i]
+#             # q_y = p_y[i-length: i]
+
+#             ax.clear()
+#             ax.set_xlim([0, 1])
+#             ax.set_ylim([0, 1])
+#             ax.text(.6, .6, steps_text[i])
+
+#             for _ in range(length):
+#                 z = max(0, i+_-length)
+#                 ax.plot(p_x[z:z+2], p_y[z:z+2], alpha=(_/length), color='black')
+            
+#             # ax.plot(q_x, q_y, alpha=.2)
+            
+#             ax.plot(triangle_x, triangle_y)
+#             ax.plot([1/3], [1/3], '--bo')
+
+#         print("Final mean", torch.mean(torch.tensor(p_x)), torch.mean(torch.tensor(p_y)))
+
+#         ani = animation.FuncAnimation(fig, animate, frames=2**steps_exp, interval=6, repeat=True)
+#         # writergif = animation.PillowWriter(fps=30)
+#         # ani.save("/home/user/Desktop/a.gif", writer=writergif)
+#         plt.show()    
+
+# def RPS_animation_NeuRD (self, total_batches_exp, steps_exp):
+#         triangle = np.array([[0, 1], [1, 0], [0, 0],[0, 1]])
+#         triangle_x = triangle[:, 0]
+#         triangle_y = triangle[:, 1]
+    
+#         p_x = []
+#         p_y = []
+
+
+#         RPS = torch.tensor([[0,-1,1],[1,0,-1],[-1,1,0]], dtype=torch.float).unsqueeze(dim=0)
+
+#         optimizer = torch.optim.SGD(x.net.parameters(), lr=.01)
+        
+#         for steps in range(2**steps_exp):
+#             for _ in range(2**(total_batches_exp-steps_exp)):
+#                 optimizer.zero_grad()
+#                 loss = x.compute_loss_from_PG_step()
+
+#                 loss.backward()
+#                 optimizer.step()
+
+#             with torch.no_grad():
+#                 logits_batch, policy_batch, value_batch = x.net.forward(RPS)
+#                 p_x.append(policy_batch[0, 0])
+#                 p_y.append(policy_batch[0, 1])
+#         fig, ax = plt.subplots()
+
+#         # Convert to time average
+#         p_x = [sum(p_x[:_]) / _ for _ in range(1, 2**steps_exp+1)]
+#         p_y = [sum(p_y[:_]) / _ for _ in range(1, 2**steps_exp+1)]
+#         steps_text = ["Param update steps: {}".format((_+1) * 2**(total_batches_exp-steps_exp)) for _ in range(2**steps_exp)]
+
+#         def animate(i):
+#             length = 10
+#             # q_x = p_x[i-length: i]
+#             # q_y = p_y[i-length: i]
+
+#             ax.clear()
+#             ax.set_xlim([0, 1])
+#             ax.set_ylim([0, 1])
+#             ax.text(.6, .6, steps_text[i])
+
+#             for _ in range(length):
+#                 z = max(0, i+_-length)
+#                 ax.plot(p_x[z:z+2], p_y[z:z+2], alpha=(_/length), color='black')
+            
+#             # ax.plot(q_x, q_y, alpha=.2)
+            
+#             ax.plot(triangle_x, triangle_y)
+#             ax.plot([1/3], [1/3], '--bo')
+
+#         print("Final mean", torch.mean(torch.tensor(p_x)), torch.mean(torch.tensor(p_y)))
+
+#         ani = animation.FuncAnimation(fig, animate, frames=2**steps_exp, interval=6, repeat=True)
+#         # writergif = animation.PillowWriter(fps=30)
+#         # ani.save("/home/user/Desktop/a.gif", writer=writergif)
+#         plt.show()  
