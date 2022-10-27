5254e06289e095c15d5cb9bb372cf9b7f178587a
Ryan <keathley.joseph.ryan@gmail.com>
2022-10-27 15:08:59 -0700

    reversion to cross conv, speed test

--- net.py
+++ net.py
@@ -3,98 +3,135 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 import time
+import logging
+import random
+
 import game
 import vtrace
 
-class Inference ():
+class CrossConv (nn.Module) :
+ 
+    def __init__ (self, size, in_channels, out_channels, device=torch.device('cpu:0'), dtype=torch.float) :
+        super().__init__()
+        self.size = size
+        self.row_conv = nn.Conv2d(in_channels, out_channels, kernel_size=(1, 2*size-1), device=device, dtype=dtype)
+        self.col_conv = nn.Conv2d(in_channels, out_channels, kernel_size=(2*size-1, 1), device=device, dtype=dtype)
 
-    def __init__ (self, logits_square=None, logits_move=None, value=None):
-        self.logits_square = logits_square
-        self.logits_move = logits_move
-        self.value = value
-
-    def flip (self):
-        self.logits_square = torch.flip(self.logits_square, dims=(1,2,3))
-        self.logits_move = torch.flip(self.logits_move, dims=(1,2,3))
+    def forward (self, input) -> torch.Tensor:
+        x = F.pad(input, (self.size-1, self.size-1, 0, 0,))
+        r = self.row_conv(x)
+        y = F.pad(input, (0, 0, self.size-1, self.size-1,))
+        c = self.col_conv(y)
+        return r + c
 
 class ConvResBlock (nn.Module):
 
-    def __init__ (self, channels, batch_norm=True, device=torch.device('cpu'), dtype=torch.float) :
+    def __init__ (self, size, channels, batch_norm=True, device=torch.device('cpu:0'), dtype=torch.float) :
         super().__init__()
-        self.conv0 = nn.Conv2d(channels, channels, kernel_size=(3,3), device=device, padding=1, dtype=dtype)
-        self.conv1 = nn.Conv2d(channels, channels, kernel_size=(3,3), device=device, padding=1 ,dtype=dtype)
+        self.conv0 = CrossConv(size, channels, channels, device=device, dtype=dtype)
+        self.conv1 = CrossConv(size, channels, channels, device=device, dtype=dtype)
         self.relu = torch.relu
-        if batch_norm:
+        if batch_norm > 0:
             self.batch_norm0 = nn.BatchNorm2d(channels, device=device, dtype=dtype)
             self.batch_norm1 = nn.BatchNorm2d(channels, device=device, dtype=dtype)
         else:
             self.batch_norm0 = nn.Identity()
             self.batch_norm1 = nn.Identity()
     
-    def forward (self, input_batch:torch.Tensor):
+    def forward (self, input_batch) -> torch.Tensor:
         return input_batch + self.batch_norm1(self.relu(self.conv1(self.batch_norm0(self.relu(self.conv0(input_batch))))))
 
 class ConvNet (nn.Module):
 
-    def __init__ (self, height, width, in_channels, channels, depth=1, batch_norm=True, device=torch.device('cpu'), dtype=torch.float) :
+    def __init__ (self, size, channels, depth=1, batch_norm=True, device=torch.device('cpu:0'), dtype=torch.float) :
         super().__init__()
         self.device = device
         self.dtype = dtype
-        self.height = height
-        self.width = width
+        self.size = size
         self.channels = channels
-        self.pre = nn.Conv2d(in_channels=in_channels, out_channels=channels, kernel_size=(3,3), padding=1, device=device, dtype=dtype)
-        self.tower = nn.ParameterList([ConvResBlock(channels, batch_norm, device=device, dtype=dtype) for _ in range(depth)])
-        self.policy_move = nn.Conv2d(channels, 16, kernel_size=(3,3), padding=1, device=device, dtype=dtype)
-        self.policy_square = nn.Conv2d(channels, 1, kernel_size=(3,3), padding=1, device=device, dtype=dtype)
-        self.value =  nn.Linear(channels * height * width, 1, device=device, dtype=dtype)
+        self.pre = CrossConv(size, in_channels=2, out_channels=channels, device=device, dtype=dtype)
+        self.tower = nn.ParameterList([ConvResBlock(size=size, channels=channels, batch_norm=batch_norm, device=device, dtype=dtype) for _ in range(depth)])
+        self.policy = nn.Linear(channels * (size**2), size, device=device, dtype=dtype)
+        self.value =  nn.Linear(channels * (size**2), 1, device=device, dtype=dtype)
 
-    def forward (self, input:torch.Tensor) -> Inference:
-        x = self.pre(input)
+    def forward (self, input_batch):
+        filter_row = input_batch[:, 1, :, 0]
+        x = input_batch
+        x = self.pre(x)
+        for block in self.tower:
+            x = block.forward(x)
+
+        x = x.view(-1, self.channels*(self.size**2))
+        logits = self.policy(x)
+        policy = F.softmax(logits, dim=1)
+        policy *= filter_row
+        F.normalize(policy, dim=1)
+        value = self.value(x)
+        actions = torch.squeeze(torch.multinomial(policy, num_samples=1))
+        return logits, policy, value, actions
+
+    def forward_policy (self, input_batch) -> torch.Tensor:
+        """
+        Does not use value head but does perform legal actions masking
+        """
+        filter_row = input_batch[:, 1, :, 0]
+        x = input_batch
+        x = self.pre(x)
         for block in self.tower:
             x = block(x)
 
-        logits_move = self.policy_move(x)
-        logits_square = self.policy_square(x)
-        value = self.value(x.view(-1, self.channels * self.height * self.width))
-        return Inference(logits_square, logits_move, value)
+        x = x.view(-1, self.channels*(self.size**2))
+        logits = self.policy(x)
+        policy = F.softmax(logits, dim=1)
+        policy *= filter_row
+        F.normalize(policy, dim=1)
+        return policy
 
-def get_size (net:torch.nn.Module):
-    param_size = 0
-    for param in net.parameters():
-        param_size += param.nelement() * param.element_size()
-    buffer_size = 0
-    for buffer in net.buffers():
-        buffer_size += buffer.nelement() * buffer.element_size()
 
-    size_all_mb = param_size + buffer_size
-    return size_all_mb / 2**20
 
 if __name__ == '__main__' :
 
-    start = time.time()
-
-    tree = game.Tree(depth_bound=3, max_transitions=2, transition_threshold=.0)
-    tree._generate()
-    tree.to(torch.device('cuda:0'))
-    print('tree chance shape', tree.chance.shape)
-
-    done_generating = time.time()
-    print('game generation time',(done_generating - start)/1)
-
-    net = ConvNet(size=tree.max_actions, channels=6, depth=3).to(device=tree.device)
-    net_ = ConvNet(size=tree.max_actions, channels=6, depth=3).to(device=tree.device)
     
+    logging.basicConfig(level=logging.DEBUG)
+
+    depth_bound_lambda = lambda tree : max(0, tree.depth_bound - (1 if random.random() < .5 else 2))
+
+    tree = game.Tree(
+        max_actions=2,
+        # depth_bound=9,
+        # max_transitions=2,
+        # depth_bound_lambda=depth_bound_lambda
+    )
+    large_saved_tree_id = "1666799117"
+
+    tree.load(large_saved_tree_id)
+    logging.debug("Tree ${} loaded, legal tensor has shape {}".format(large_saved_tree_id, tree.legal.shape))
+    # tree._assert_index_is_tree()
+    tree.to(torch.device('cuda:0'))
+
+   
     batch_size = 2**1
+    net_channels=32
+    net_depth=1
+    net =  ConvNet(size=tree.max_actions, channels=net_channels, depth=net_depth, device=tree.device)
+    net_ = ConvNet(size=tree.max_actions, channels=net_channels, depth=net_depth, device=tree.device)
+
     episodes = game.Episodes(tree, batch_size)
     episodes.generate(net)
-
     vtrace.transform_rewards(episodes, net, net_, net_, .2)
 
+    # vtrace.estimate(episodes,reward_transform=None)
 
-    vtrace.estimate(episodes,reward_transform=None)
-
-    done_stepping = time.time()
-    print('episode generation time', (done_stepping - done_generating)/1)
-
-
+    def speed_test (net):
+        start_generating = time.perf_counter()
+        # Speed test
+        batch_size = 10**4
+        steps = 0
+        for trial in range(100):
+            episodes = game.Episodes(tree, batch_size)
+            episodes.generate(net)
+            steps += episodes.t_eff * batch_size
+        
+        end_generating = time.perf_counter()
+        speed = steps / (end_generating - start_generating)
+        logging.debug('{} steps/sec'.format(int(speed)))
