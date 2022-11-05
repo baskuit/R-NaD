NashConv revamp, fix policy normalization

metric.py
@@ -13,32 +13,35 @@ inference_batch_size is for splitting very large games into chunks that fit into
 By default we store game-wide tensors on cpu
 """
 
+class NashConvData ():
+
+    def __init__ (self, tree: game.Tree):
+        self.policy = torch.zeros((tree.size, 2 * tree.max_actions), device=tree.device, dtype=torch.double)
+        self.max_1 =  torch.zeros((tree.size,), device=tree.device, dtype=torch.double)
+        self.min_2 =  torch.zeros((tree.size,), device=tree.device, dtype=torch.double)
+        self.depth =  torch.zeros((tree.size,), device=tree.device, dtype=torch.int)
+
 def nash_conv (tree : game.Tree, net : net.ConvNet, inference_batch_size=10**5) :
 
+    data = NashConvData(tree)
+    data.policy = tree.nash
+
     net.eval()
-
-    tree_size = tree.value.shape[0]
-    policy = torch.zeros((tree_size, 2 * tree.max_actions), device=torch.device('cpu'))
-
-    for _ in range(tree_size // inference_batch_size + 1):
-        slice_range = torch.arange(_ * inference_batch_size, min((_+1) * inference_batch_size, tree_size), device=tree.device)
+    for _ in range(tree.size // inference_batch_size + 1):
+        slice_range = torch.arange(_ * inference_batch_size, min((_+1) * inference_batch_size, tree.size), device=tree.device)
         value_slice = tree.expected_value[slice_range]
         legal_slice = tree.legal[slice_range]
 
-        inference_slice = torch.cat([value_slice, legal_slice], dim=1)
         with torch.no_grad():
-            policy[slice_range, :tree.max_actions] = net.forward_policy(inference_slice).to(torch.device('cpu'))
+            inference_slice = torch.cat([value_slice, legal_slice], dim=1)
+            data.policy[slice_range, :tree.max_actions] = net.forward_policy(inference_slice).to(torch.device('cpu'))
             inference_slice = torch.cat([-value_slice, legal_slice], dim=1).swapaxes(2, 3)
-            policy[slice_range, tree.max_actions:] = net.forward_policy(inference_slice).to(torch.device('cpu'))
-
+            data.policy[slice_range, tree.max_actions:] = net.forward_policy(inference_slice).to(torch.device('cpu'))
+    max_min(tree, data)
     net.train()
-    print(policy[1])
-    max_1, min_2 = max_min(tree, policy)
+    return data
 
-    exploitability = max_1 - min_2
-    return exploitability
-
-def max_min (tree : game.Tree, policy : torch.Tensor, root_index=1, depth=0):
+def max_min (tree : game.Tree, data : NashConvData, root_index=1, depth=0):
 
     matrix_1 = torch.zeros((tree.max_transitions * tree.max_actions**2,))
     matrix_2 = torch.zeros((tree.max_transitions * tree.max_actions**2,))
@@ -48,6 +51,7 @@ def max_min (tree : game.Tree, policy : torch.Tensor, root_index=1, depth=0):
     chance_root = torch.flatten(tree.chance[root_index:root_index+1] * tree.legal[root_index:root_index+1])
 
     places = (chance_root > 0).nonzero()
+    depths = [0]
     for idx_flat in places:
 
         transition_prob = chance_root[idx_flat]
@@ -56,7 +60,10 @@ def max_min (tree : game.Tree, policy : torch.Tensor, root_index=1, depth=0):
             v = value_root[idx_flat]
             max_1_, min_2_ = v, v
         else:
-            max_1_, min_2_ = max_min (tree, policy, root_index_, depth=depth+1)
+            max_min(tree, data, root_index_, depth=depth+1)
+            max_1_ = data.max_1[root_index_]
+            min_2_ = data.min_2[root_index_]
+            depths.append(data.depth[root_index_])
 
         matrix_1[idx_flat] = (max_1_ * transition_prob).to(torch.float)
         matrix_2[idx_flat] = (min_2_ * transition_prob).to(torch.float)
@@ -66,34 +73,56 @@ def max_min (tree : game.Tree, policy : torch.Tensor, root_index=1, depth=0):
     matrix_1 = torch.sum(matrix_1, dim=0)
     matrix_2 = torch.sum(matrix_2, dim=0)
 
-    pi = policy[root_index]
-
+    pi = data.policy[root_index]
     pi_1, pi_2 = pi[:tree.max_actions].unsqueeze(dim=0), pi[tree.max_actions:].unsqueeze(dim=1)
-
     row_actions_mask = tree.legal[root_index, 0, :, 0].to(torch.bool)
     col_actions_mask = tree.legal[root_index, 0, 0, :].to(torch.bool)
     prod_1 = torch.flatten(torch.matmul(matrix_1, pi_2))[row_actions_mask]
     prod_2 = torch.flatten(torch.matmul(pi_1, matrix_2))[col_actions_mask]
-    max_1 = torch.max(prod_1).item()
-    min_2 = torch.min(prod_2).item()
+    data.max_1[root_index] = torch.max(prod_1)
+    data.min_2[root_index] = torch.min(prod_2)
 
-    return max_1, min_2
+    depth = 1 + max(depths)
+    data.depth[root_index] = depth
 
+def mean_nash_conv_by_depth (data : NashConvData):
+
+    max_depth = int(data.depth[1].item())
+    nash_conv = data.max_1 - data.min_2
+    means = {}
+    for depth in range(1, max_depth + 1):
+        idx = (data.depth == depth)
+        expls = nash_conv[idx]
+        means[depth] = torch.mean(expls).item()
+    return means
 
     
 
 if __name__ == '__main__' :
     import game
     import net
-    tree = game.Tree()
-    tree.load('1667264620')
+    tree = game.Tree(
+        depth_bound=2,
+        max_actions=3,
+    )
+    # tree.load('1667264620')
+    tree._generate()
     tree._assert_index_is_tree()
-    # net = net.ConvNet(tree.max_actions, 1, 1)
+    net_ = net.ConvNet(tree.max_actions, 1, 1)
     # expl = nash_conv(tree, net, 1000)
     # print(expl)
-    pi_1,pi_2 = tree.legal[:, 0, :, 0], tree.legal[:, 0, 0, :]
-    pi_1 = torch.nn.functional.normalize(pi_1, dim=1, p=1)
-    pi_2 = torch.nn.functional.normalize(pi_2, dim=1, p=1)
-    pi = torch.cat([pi_1, pi_2], dim=1)
-    print(pi[:10])
-    print(max_min(tree, pi, 1))
+    # pi_1, pi_2 = tree.legal[:, 0, :, 0], tree.legal[:, 0, 0, :]
+    # pi_1 = torch.nn.functional.normalize(pi_1, dim=1, p=1)
+    # pi_2 = torch.nn.functional.normalize(pi_2, dim=1, p=1)
+
+    # pi = tree.nash
+    # pi_1, pi_2 = pi[:, :3], pi[:, 3:]
+    # pi = torch.cat([pi_1, pi_2], dim=1)
+    data = nash_conv(tree, net_)
+    means = mean_nash_conv_by_depth(data)
+    for _, __ in means.items():
+        print(_, __)
+    # for _ in range(tree.size):
+    #     print(_)
+    #     print(tree.expected_value[_])
+    #     print(tree.nash[_])

net.py
@@ -86,7 +86,7 @@ class ConvNet (nn.Module):
         logits = self.policy(x)
         policy = F.softmax(logits, dim=1)
         policy *= filter_row
-        F.normalize(policy, dim=1, p=1)
+        policy = F.normalize(policy, dim=1, p=1)
         return policy
 
     def forward_batch (self, episodes : game.Episodes) :