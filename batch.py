1fe15c839065eae5d34ef942a54f20446220608d
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-02-07 18:25:28 -0700

    move search to batch temp. add search to generate

--- batch.py
+++ batch.py
@@ -3,12 +3,129 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from game import Tree
+# from search import Search
+
 from collections import deque
 from typing import List
 
 import time
 import random
 import numpy
+
+
+class Search:
+
+    """
+    A tree-wide search can be simlutated by talking value inferences of the tree
+    and applying a map that recursively replaces these values with the NE payoff
+    of the matrix of previous value estimates.
+
+    We can also test my imperfect information look-ahead idea. No explanation yet, sorry.
+    """
+
+    def __init__(self, tree: Tree, same_info_prob=.7):
+        self.tree = tree
+        self.size = tree.value.shape[0]
+        self.policy = torch.zeros_like(tree.solution)
+        self.value = torch.zeros((self.size, 2), device=tree.device, dtype=torch.float)
+        same_info_prng = torch.rand_like(self.value)
+        self.same_info = same_info_prng < same_info_prob
+
+    def to(self, device):
+        for key, value in self.__dict__.items():
+            if torch.is_tensor(value):
+                self.__dict__[key] = value.to(device)
+
+
+    def apply_net(self, net: torch.nn.Module, inference_batch_size=10**5):
+
+        net.eval()
+        for _ in range(self.size // inference_batch_size + 1):
+            slice_range = torch.arange(
+                _ * inference_batch_size,
+                min((_ + 1) * inference_batch_size, self.size),
+                device=self.tree.device,
+            )
+            value_slice = self.tree.expected_value[slice_range]
+            legal_slice = self.tree.legal[slice_range]
+
+            with torch.no_grad():
+                inference_slice_row = torch.cat([value_slice, legal_slice], dim=1).contiguous()
+                _, policy, value, _ = net.forward(inference_slice_row)
+                self.policy[slice_range, : self.tree.max_actions] = policy
+                self.value[slice_range, 0] = value[:, 0]
+                inference_slice_col = torch.cat([-value_slice, legal_slice], dim=1).swapaxes(
+                    2, 3
+                ).contiguous()
+                _, policy, value, _ = net.forward(inference_slice_col)
+                self.policy[slice_range, self.tree.max_actions : ] = policy
+                self.value[slice_range, 1] = value[:, 0]
+
+    def step(self, idx_cur=1) -> torch.Tensor:
+
+
+        matrix_1 = torch.zeros(
+            (self.tree.max_transitions * self.tree.max_actions**2,),
+            device=self.tree.device,
+            dtype=torch.float,
+        )
+        matrix_2 = torch.zeros_like(matrix_1)
+
+        value = torch.flatten(
+            self.tree.value[idx_cur : idx_cur + 1]
+        )
+        index = torch.flatten(
+            self.tree.index[idx_cur : idx_cur + 1]
+        )
+        chance = torch.flatten(
+            self.tree.chance[idx_cur : idx_cur + 1]
+            * self.tree.legal[idx_cur : idx_cur + 1]
+        )
+        # tensors at the root, shape (1, t, a, a)
+
+        subtrees = (chance > 0).nonzero()
+        for idx_local in subtrees:
+            idx_local = idx_local[0]
+            idx_next = index[idx_local]
+            if idx_next == 0:
+                v1 = value[idx_local]
+                v2 = -value[idx_local]
+            else:
+                v1, v2 = self.step(idx_next)
+                # v1 = self.value[idx_next, 0]
+                # v2 = self.value[idx_next, 1]
+                # Recursive call just before using update values
+            transition_prob = chance[idx_local]
+            matrix_1[idx_local] = v1 * transition_prob
+            matrix_2[idx_local] = v2 * transition_prob
+
+        matrix_1 = matrix_1.view(self.tree.max_transitions, self.tree.max_actions, self.tree.max_actions)
+        matrix_2 = matrix_2.view(self.tree.max_transitions, self.tree.max_actions, self.tree.max_actions)
+        matrix_1 = torch.sum(matrix_1, dim=0)
+        matrix_2 = torch.sum(matrix_2, dim=0)
+        # Add to get expected value estimates and reshape into matrix shape
+
+        solution_1 = self.tree._solve( matrix_1, self.tree.max_actions)[0]
+        solution_2 = self.tree._solve(-matrix_2, self.tree.max_actions)[0]
+        # Solve NE in both players search trees
+
+        pi_1, pi_2 = solution_1[: self.tree.max_actions].unsqueeze(dim=0), solution_1[
+            self.tree.max_actions :
+        ].unsqueeze(dim=1)
+        self.value[idx_cur, 0] = torch.matmul(
+            torch.matmul(pi_1, matrix_1), pi_2
+        )
+        self.policy[idx_cur, : self.tree.max_actions] = torch.flatten(pi_1)
+        pi_1, pi_2 = solution_2[: self.tree.max_actions].unsqueeze(dim=0), solution_2[
+            self.tree.max_actions :
+        ].unsqueeze(dim=1)
+        old_value = self.value[idx_cur].clone()
+        self.value[idx_cur, 1] = torch.matmul(
+            torch.matmul(pi_1, matrix_2), pi_2
+        )
+        self.policy[idx_cur, self.tree.max_actions : ] = torch.flatten(pi_2)
+        return old_value
+        # Get payoff that serves as new values
 
 
 class States:
@@ -135,7 +252,7 @@ class Episodes:
         result.t_eff = t_eff
         return result
 
-    def generate(self, net: torch.nn.Module):
+    def generate(self, net: torch.nn.Module, search_depth=0,):
         """
         Play a batch of episodes with a given actor net
         """
@@ -151,6 +268,11 @@ class Episodes:
         net.eval()
         time_start = time.perf_counter()
 
+        search = Search(self.tree, same_info_prob=.7) # info probs does nothing currently
+        search.apply_net(net)
+        for depth in range(search_depth):
+            search.step()
+
         while not self.states.terminal:
 
             indices_list.append(self.states.indices.clone())
@@ -158,9 +280,21 @@ class Episodes:
 
             observations = self.states.observations()
 
-            with torch.no_grad():
-                logits, policy, value, actions = net.forward(observations)
+            # with torch.no_grad():
+            #     logits, policy, value, actions = net.forward(observations)
             # actions = torch.zeros_like(policy)
+            policy = search.policy
+            policy = torch.index_select(policy, dim=0, index=self.states.indices)
+            policy_1 = policy[:, : self.tree.max_actions]
+            policy_2 = policy[:, self.tree.max_actions :]
+            policy = torch.stack((policy_1, policy_2), dim=1)
+            policy = policy[torch.arange(self.batch_size), self.states.turns]
+            value = torch.index_select(search.value, dim=0, index=self.states.indices)
+            value = value[torch.arange(self.batch_size), self.states.turns]
+            # TODO investiage whether you should use the net values or solved values
+            # my guess: the solved values should be just fine
+            actions = torch.squeeze(torch.multinomial(policy, num_samples=1))
+
             rewards = self.states.step(actions)
             actions_oh = torch.zeros_like(policy)
             actions_oh[torch.arange(self.batch_size), actions] = 1
